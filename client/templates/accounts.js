698529d882eaee1bb8ba9daedc4ad8d3500129a2
Alex Bierach <alex@thebrandingfarm.com>
2015-11-14 09:46:17 -0800

    adds jquery validator plugin; implements account registration and login validation

--- client/templates/accounts.js
+++ client/templates/accounts.js
@@ -3,7 +3,6 @@ Template.loginRegister.events({
     'click .login-btn': function() {
     	Session.set('registering', false);
     	Session.set('loggingIn', true);
-    	console.log(Session.get('loggingIn'));
     },
     'click .register-btn': function() {
     	Session.set('registering', true);
@@ -23,27 +22,98 @@ Template.loginRegister.helpers({
 Template.register.events({
     'submit form': function(event) {
         event.preventDefault();
-        var emailVar = event.target.registerEmail.value;
-        var passwordVar = event.target.registerPassword.value;
-        Accounts.createUser({
-            email: emailVar,
-            password: passwordVar
-        });
     }
+});
+
+Template.register.onRendered(function(){
+   var validator = $('.register').validate({
+        submitHandler: function(event) {
+            var email = $('[name=email]').val();
+            var password = $('[name=password]').val();
+            
+            Accounts.createUser({
+                email: email,
+                password: password
+            }, function(error) {
+                if (error) {
+                    if(error.reason == 'Email already exists.'){
+                        validator.showErrors({
+                            email: 'That email already belongs to a registered user.'   
+                        });
+                    }
+                } else {
+                    Router.go('home');
+                }
+            });
+        }
+    });
 });
 
 Template.login.events({
     'submit form': function(event){
         event.preventDefault();
-        var emailVar = event.target.loginEmail.value;
-        var passwordVar = event.target.loginPassword.value;
-        Meteor.loginWithPassword(emailVar, passwordVar, function(err) {
-        	      if (err) {
-        	      	console.log('oops, wrong');
-        	      } else{ 
+    }
+});
 
-        	      }
-        });
+Template.login.onCreated(function(){
+    console.log("The 'login' template was just created.");
+});
+
+Template.login.onRendered(function(){
+    console.log("The 'login' template was just rendered.");
+    var validator = $('.login').validate({
+        submitHandler: function(event) {
+            var email = $('[name=email]').val();
+            var password = $('[name=password]').val();
+            
+            Meteor.loginWithPassword(email, password, function(error) {
+                if (error) {
+                    if (error.reason == 'User not found') {
+                        validator.showErrors({
+                            email: error.reason
+                        });
+                    }
+                    if (error.reason == 'Incorrect password') {
+                        validator.showErrors({
+                            password: error.reason
+                        });
+                    }
+                } else { 
+                    var currentRoute = Router.current().route.getName();
+                    if(currentRoute == "login"){
+                        Router.go("home");
+                    }
+                }
+            });
+        }
+    });
+});
+
+Template.login.onDestroyed(function(){
+    console.log("The 'login' template was just destroyed.");
+});
+
+
+$.validator.setDefaults({
+    rules: {
+        email: {
+            required: true,
+            email: true
+        },
+        password: {
+            required: true,
+            minlength: 6
+        }
+    },
+    messages: {
+        email: {
+            required: "You must enter an email address.",
+            email: "You've entered an invalid email address."
+        },
+        password: {
+            required: "You must enter a password.",
+            minlength: "Your password must be at least {0} characters."
+        }
     }
 });
 
--- client/templates/dashboard.js
+++ client/templates/dashboard.js
@@ -2,5 +2,6 @@ Template.dashboard.events({
     'click .logout': function(event){
         event.preventDefault();
         Meteor.logout();
+        Router.go('login');
     }
 });
