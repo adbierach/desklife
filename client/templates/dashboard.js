1b36dbab29d3f3d93c48ca7f0fba751cd1eea850
Alex Bierach <alex@thebrandingfarm.com>
2015-11-14 12:31:37 -0800

    adds reset routines button to dashboard; adds reset routines method to clear completed routines

--- client/templates/dashboard.js
+++ client/templates/dashboard.js
@@ -3,5 +3,10 @@ Template.dashboard.events({
         event.preventDefault();
         Meteor.logout();
         Router.go('login');
+    },
+
+    'click .clear-routines': function(event) {
+    	event.preventDefault();
+    	Meteor.call('clearCompletedRoutines');
     }
 });
--- server/users.js
+++ server/users.js
@@ -6,5 +6,12 @@ Meteor.methods({
 			throw new Meteor.error('not-logged-in', 'You\'re not logged in.');
 		} 
 		Meteor.users.update({_id: currentUser}, {$addToSet: {routinesCompletedToday: routineId}});
+	},
+	'clearCompletedRoutines' : function () {
+		var currentUser = Meteor.userId();
+		if(!currentUser) {
+			throw new Meteor.error('not-logged-in', 'You\'re not logged in.');
+		} 
+		Meteor.users.update({_id: currentUser}, {$set : {routinesCompletedToday: []}});
 	}
 });
